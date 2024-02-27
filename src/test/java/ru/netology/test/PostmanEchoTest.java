d5851ec8e0287fd83e24d19bd6ce44f1d6ea8ea3
Egorius188 <postulgin98@mail.ru>
2024-02-27 16:10:32 +0300

    initial commit

---
+++ src/test/java/ru/netology/test/PostmanEchoTest.java
@@ -0,0 +1,23 @@
+package ru.netology.test;
+
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.equalTo;
+
+class PostmanEchoTest {
+    @Test
+    void shouldReturnData() {
+        var text = "Hello";
+        given()
+                .contentType("text/plain; charset=UTF-8")
+                .baseUri("https://postman-echo.com")
+                .body(text)
+                .when()
+                .post("/post")
+                .then()
+                .statusCode(200)
+                .body("data", equalTo(text))
+        ;
+    }
+}
